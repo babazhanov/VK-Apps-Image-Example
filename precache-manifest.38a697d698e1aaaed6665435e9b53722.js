213e258228e228ffb44620f780aa098206e95211
Ruslan Babazhanov <babazhanov@gmail.com>
2020-09-15 11:12:34 +0300

    Updates

---
+++ precache-manifest.38a697d698e1aaaed6665435e9b53722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22585cae9c90812cc6471871f00d358",
+    "url": "/VK-Apps-Image-Example/index.html"
+  },
+  {
+    "revision": "b93e8d188d911efe33c3",
+    "url": "/VK-Apps-Image-Example/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "b93e8d188d911efe33c3",
+    "url": "/VK-Apps-Image-Example/static/js/2.ba8d2a3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VK-Apps-Image-Example/static/js/2.ba8d2a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9975da449780c1f3f2d5",
+    "url": "/VK-Apps-Image-Example/static/js/main.cbb3ec34.chunk.js"
+  },
+  {
+    "revision": "fdc8e3582e326e82ea82",
+    "url": "/VK-Apps-Image-Example/static/js/runtime-main.eb1b30a3.js"
+  }
+]);
